f5ab37d34e57b28dbc7cb470ba32048e17ba87e5
ﷲﷴ﷼ﯼ <joaomotionless570@gmail.com>
2024-05-23 13:35:50 -0300

    Add files via upload

---
+++ cisco 1.py
@@ -0,0 +1,9 @@
+#sep = separador de string 
+print ("Meu", "nome", "é", "joão.\n", sep="-", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programação","Essenciais", "em", sep="***", end="...")
+print("Python")
+
+print("Meu\nnome\né\joão.", end=" ")
+print("james joao.")
